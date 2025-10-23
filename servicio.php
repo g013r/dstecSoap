905a092debcdec7927e805a1eda391debc4100da
g013r <hectorsegundo23q>
2025-10-23 01:18:22 -0500

    modelo servicio

---
+++ servicio.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Modelo;
+
+class servicio {
+    
+    public $id_servicio;
+    public $nombre;
+    public $descripcion;
+    public $precio;
+    public $estado;
+    
+    public function __construct(array $datos = []) {
+        if (!empty($datos)) {
+            $this->id_servicio = $datos['id_servicio'] ?? null;
+            $this->nombre = $datos['nombre'] ?? null;
+            $this->descripcion = $datos['descripcion']?? null;
+            $this->precio = $datos['precio']?? null;
+            $this->estado = $datos['estado']?? null;
+        }
+    }
+     
+}
