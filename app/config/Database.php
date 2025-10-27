98a7084a81b0ea8ab404bf65cd33d3d5b07c8166
g013r <hectorsegundo23q>
2025-10-26 19:28:26 -0500

    Conexion a la base de datos

---
+++ app/config/Database.php
@@ -0,0 +1,37 @@
+<?php
+
+class Database{
+    private $host = "localhost";
+    private $db_name = "dstec";
+    private $username = "root";
+    private $password = "";
+
+    private $con;
+
+    //singleton para la conexion a la base de datos
+    private static $instance = null;
+
+    //constructor de la clase para la conexion a la base de datos
+    private function __construct(){
+        $this->con = null;
+        try{
+            $this->con = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            echo "Error de conexión: " . $e->getMessage();
+        }
+    }
+
+    //metodo para obtener la instancia de la clase
+    public static function getInstance(){
+        if(self::$instance === null){
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    //metodo para obtener la conexion a la base de datos
+    public function getConnection(){
+        return $this->con;
+    }
+}
